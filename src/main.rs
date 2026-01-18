a047dc50acb3ed8a9c2e2c04d13266795073eeab
Merlin Clos <92889964+Merlin-Clos@users.noreply.github.com>
2026-01-18 18:22:39 -0500

    feat/init makefile2doc following minigrep rust book (#1)

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use makefile2doc::search;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Error: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+        let file_path = args[1].clone();
+
+        if file_path != "Makefile" {
+            return Err("makedoc only make the documentation for Makefiles");
+        }
+
+        Ok(Config { file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = search(&contents);
+
+    if results.is_empty() {
+        return Err("No documented targets found in this Makefile".into());
+    }
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
