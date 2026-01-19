da8b8e6c97e076b48d583e2dbfa5d2575222a572
Merlin Clos <92889964+Merlin-Clos@users.noreply.github.com>
2026-01-18 21:04:54 -0500

    feat: implement convention and test of generate doc (#3)

---
+++ tests/integration.rs
@@ -0,0 +1,10 @@
+use makefile2doc;
+use std::fs;
+
+#[test]
+fn test_generate_documentation() {
+    let input = fs::read_to_string("tests/fixtures/Makefile").unwrap();
+    let expected = fs::read_to_string("tests/fixtures/expected.md").unwrap();
+    
+    assert_eq!(input, expected);
+}
