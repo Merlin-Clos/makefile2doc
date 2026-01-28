76f50ed424b8d4c0f21908a53d32df36b54e6695
Merlin-Clos <Merlin-Clos@users.noreply.github.com>
2026-01-28 07:53:56 +0000

    deploy: 957673dd9b4dd1547f1bae3289a5a90ab0576459

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d6041463.js',
+                path_to_root + 'searchindex-90a8464f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
